620e4cfd54e8d857bb824eaaac0a13090b0eb858
skverskk <ken.skversky@gmail.com>
2019-06-06 17:56:15 -0400

    Finished Product

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,15 +1,29 @@
+import { MovieService } from './../../services/movie.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
-  styleUrls: ['./movie-details.page.scss'],
+  styleUrls: ['./movie-details.page.scss']
 })
 export class MovieDetailsPage implements OnInit {
+  information = null;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private movieService: MovieService
+  ) {}
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.movieService.getDetails(id).subscribe(result => {
+      console.log('Details: ', result);
+      this.information = result;
+    });
   }
 
+  openWebsite() {
+    window.open(this.information.Website, '_blank');
+  }
 }
